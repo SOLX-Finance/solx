8592de2003871bc45738bfc3860c10cf1ae17000
kostyamospan <kostya04042001@gmail.com>
2025-04-23 23:03:35 +0300

    fix: user wallet_address column name

--- libs/data-access/prisma/schema/migrations/20250423200316_sale_tag_file_tables/migration.sql
+++ libs/data-access/prisma/schema/migrations/20250423200316_sale_tag_file_tables/migration.sql
@@ -5,6 +5,7 @@
   - You are about to drop the column `first_name` on the `user` table. All the data in the column will be lost.
   - You are about to drop the column `last_name` on the `user` table. All the data in the column will be lost.
   - You are about to drop the column `password` on the `user` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[wallet_address]` on the table `user` will be added. If there are existing duplicate values, this will fail.
 
 */
 -- CreateEnum
@@ -15,8 +16,8 @@ ALTER TABLE "user" DROP COLUMN "avatar_url",
 DROP COLUMN "first_name",
 DROP COLUMN "last_name",
 DROP COLUMN "password",
-ADD COLUMN     "address" TEXT,
-ADD COLUMN     "profilePictureId" TEXT;
+ADD COLUMN     "profile_picture_id" TEXT,
+ADD COLUMN     "wallet_address" TEXT;
 
 -- CreateTable
 CREATE TABLE "file" (
@@ -50,5 +51,8 @@ CREATE UNIQUE INDEX "file_remote_id_key" ON "file"("remote_id");
 -- CreateIndex
 CREATE UNIQUE INDEX "sale_tag_name_key" ON "sale_tag"("name");
 
+-- CreateIndex
+CREATE UNIQUE INDEX "user_wallet_address_key" ON "user"("wallet_address");
+
 -- AddForeignKey
-ALTER TABLE "user" ADD CONSTRAINT "user_profilePictureId_fkey" FOREIGN KEY ("profilePictureId") REFERENCES "file"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "user" ADD CONSTRAINT "user_profile_picture_id_fkey" FOREIGN KEY ("profile_picture_id") REFERENCES "file"("id") ON DELETE SET NULL ON UPDATE CASCADE;

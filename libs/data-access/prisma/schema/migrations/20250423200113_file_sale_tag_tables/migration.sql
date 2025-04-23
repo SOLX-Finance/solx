155ae47678f864a018f58209af194b669a2b7a67
kostyamospan <kostya04042001@gmail.com>
2025-04-23 23:01:58 +0300

    feat: file and saletag tables

---
+++ libs/data-access/prisma/schema/migrations/20250423200113_file_sale_tag_tables/migration.sql
@@ -0,0 +1,54 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `avatar_url` on the `user` table. All the data in the column will be lost.
+  - You are about to drop the column `first_name` on the `user` table. All the data in the column will be lost.
+  - You are about to drop the column `last_name` on the `user` table. All the data in the column will be lost.
+  - You are about to drop the column `password` on the `user` table. All the data in the column will be lost.
+
+*/
+-- CreateEnum
+CREATE TYPE "FileType" AS ENUM ('PROFILE_PICTURE', 'SALE_CONTENT', 'SALE_DEMO', 'SALE_PREVIEW');
+
+-- AlterTable
+ALTER TABLE "user" DROP COLUMN "avatar_url",
+DROP COLUMN "first_name",
+DROP COLUMN "last_name",
+DROP COLUMN "password",
+ADD COLUMN     "address" TEXT,
+ADD COLUMN     "profilePictureId" TEXT;
+
+-- CreateTable
+CREATE TABLE "file" (
+    "id" TEXT NOT NULL,
+    "type" "FileType" NOT NULL,
+    "remote_id" TEXT NOT NULL,
+    "mime_type" TEXT NOT NULL,
+    "ai_reviewed" BOOLEAN NOT NULL DEFAULT false,
+    "ai_review_details" JSONB,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "file_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "sale_tag" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "color" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "sale_tag_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "file_remote_id_key" ON "file"("remote_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "sale_tag_name_key" ON "sale_tag"("name");
+
+-- AddForeignKey
+ALTER TABLE "user" ADD CONSTRAINT "user_profilePictureId_fkey" FOREIGN KEY ("profilePictureId") REFERENCES "file"("id") ON DELETE SET NULL ON UPDATE CASCADE;

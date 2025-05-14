7b15bacb4f14aa381aeffd038506f72baeab913e
kostyamospan <kostya04042001@gmail.com>
2025-05-14 22:07:26 +0300

    feat: heroku deploy

---
+++ heroku-build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+echo "BUILD_APP environment variable is set to: ${BUILD_APP:-not set}"
+
+if [ -z "$BUILD_APP" ]; then
+    echo "Error: BUILD_APP environment variable is not set"
+    exit 1
+fi
+
+case "$BUILD_APP" in
+    "api")
+        yarn db:generate && yarn build:api
+        ;;
+    "indexer")
+        yarn db:generate && yarn build:indexer
+        ;;
+    *)
+        echo "Error: Invalid BUILD_APP value. Allowed values: 'api' or 'indexer'
+        exit 1
+        ;;
+esac

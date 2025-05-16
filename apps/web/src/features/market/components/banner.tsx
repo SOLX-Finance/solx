03edbd78ab8debcec64550f2b8e76ab298fe50f4
MrJeleika <den932005@gmail.com>
2025-05-16 18:59:47 +0300

    adaptive makrets page

--- apps/web/src/features/market/components/banner.tsx
+++ apps/web/src/features/market/components/banner.tsx
@@ -4,7 +4,7 @@ import discoverDapps from '../../../assets/logo/discover-dapps.svg';
 export const Banner = () => {
   return (
     <div className="grid grid-cols-3 gap-[20px]">
-      <div className="col-span-2">
+      <div className="md:col-span-2 col-span-3">
         <div className="w-full h-[360px] min-h-[200px] bg-[#BA8FFF] rounded-[40px] relative">
           <img src={cryptoNews} alt="news" className="absolute right-0 top-0" />
           <div className="absolute bottom-[30px] left-[30px] text-white text-[40px] font-semibold">
@@ -14,7 +14,7 @@ export const Banner = () => {
           </div>
         </div>
       </div>
-      <div>
+      <div className="max-md:hidden">
         <div className="w-full h-[360px] min-h-[200px] bg-black rounded-[40px] relative">
           <img
             src={discoverDapps}

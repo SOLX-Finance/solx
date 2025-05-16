3adb643fcd1673e9a2ca8ce41db9c419dd8f7032
Dmytro Horbatenko <dmytro.horbatenko@redduck.io>
2025-05-16 17:08:26 +0300

    fix: mobile

--- apps/web/src/features/sales/components/SalePage/ActionButtons.tsx
+++ apps/web/src/features/sales/components/SalePage/ActionButtons.tsx
@@ -14,11 +14,11 @@ const ActionButtons: React.FC<ActionButtonsProps> = ({
   onDownloadDemo,
 }) => {
   return (
-    <div className="flex gap-5">
+    <div className="flex gap-5 flex-col md:flex-row">
       {!hasBuyer && (
         <button
           onClick={onPurchase}
-          className="bg-[#C4E703] border border-black text-black font-medium text-2xl py-2.5 px-10 rounded-full hover:bg-[#d1f033] transition-colors"
+          className="bg-[#C4E703] min-h-[54px] border border-black text-black font-medium text-xl md:text-2xl py-1.5 px-4 md:py-2.5 md:px-10 rounded-full hover:bg-[#d1f033] transition-colors"
         >
           Buy
         </button>
@@ -27,7 +27,7 @@ const ActionButtons: React.FC<ActionButtonsProps> = ({
       {hasDemoFile && (
         <button
           onClick={onDownloadDemo}
-          className="border border-black text-black font-medium text-2xl py-2.5 px-10 rounded-full hover:bg-gray-100 transition-colors"
+          className="border border-black min-h-[54px] text-black font-medium text-2xl py-2.5 px-10 rounded-full hover:bg-gray-100 transition-colors"
         >
           Download Demo
         </button>
